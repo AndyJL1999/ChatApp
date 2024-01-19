549b546b3d711f5bb6a7aae135ae4cff885abeae
AndyJL1999 <andylainez115@hotmail.com>
2024-01-18 19:19:04 -0500

    --Added Models folder

---
+++ ChatApp.DataAccess/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string? GroupId { get; set; }
+        public string? ChatId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        public DateTime? DeliveredAt { get; set; }
+        public DateTime? SeenAt { get; set; }
+    }
+}
---
+++ ChatApp.DataAccess/Models/UserChat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Models
+{
+    public class UserChat
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ChatId { get; set; }
+    }
+}
