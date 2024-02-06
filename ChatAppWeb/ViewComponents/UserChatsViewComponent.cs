cabbcc3e118bcc2f1695dc34dd09b0ef339ccc6a
AndyJL1999 <andylainez115@hotmail.com>
2024-02-05 21:43:55 -0500

    --Created UserChatsViewComponent.cs --Created ViewComponents folder

---
+++ ChatAppWeb/ViewComponents/UserChatsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppWeb.ViewComponents
+{
+    public class UserChatsViewComponent : ViewComponent
+    {
+        public UserChatsViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
