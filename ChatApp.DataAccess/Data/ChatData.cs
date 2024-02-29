ceeda3bd1708bff3493bf37a39e0452b649a4938
AndyJL1999 <andylainez115@hotmail.com>
2024-02-28 21:11:34 -0500

    --Created ChatData class --Created IChatData interface --Modified SqlDataAccess and ISqlDataAccess

---
+++ ChatApp.DataAccess/Data/ChatData.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.DataAccess.DataAccess;
+using ChatApp.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Data
+{
+    public class ChatData : IChatData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public ChatData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task UpsertChat(string id, string name) =>
+            _db.SaveData("spChat_Upsert", new { Id = id, Name = name });
+        
+    }
+}
---
+++ ChatApp.DataAccess/Interfaces/IChatData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Interfaces
+{
+    public interface IChatData
+    {
+        Task UpsertChat(string id, string name);
+    }
+}
