49a75fd2dfda4977d5ac8d1f747e74ec33345573
AndyJL1999 <andylainez115@hotmail.com>
2024-02-28 21:08:16 -0500

    --Created spChat_Upsert stored procedure

---
+++ ChatAppDb/dbo/StoredProcedures/Chat_Procedures/spChat_Upsert.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spChat_Upsert]
+	@Id nvarchar(450),
+	@Name nvarchar(256)
+AS
+
+if((select 1 from dbo.[chat] where Id = @Id) = 1)
+begin
+	update dbo.[chat]
+	set Name = @Name
+	where Id = @Id;
+end
+else
+begin
+	insert into dbo.[chat](Id, Name)
+	values(@Id, @Name)
+end
+
