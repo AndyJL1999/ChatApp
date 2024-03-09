a76b31d73ab8a402d2b00b81d7bfee29d7f14fb4
AndyJL1999 <andylainez115@hotmail.com>
2024-03-08 21:05:50 -0500

    --Created AuthController --Created both Login and Register Views

---
+++ ChatAppWeb/Areas/UserAccount/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using ChatApp.DataAccess.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppWeb.Areas.UserAccount.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserVM user)
+        {
+            return View("UserHome/Views/Index");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserVM user)
+        {
+            return View("UserHome/Views/Index");
+        }
+    }
+}
