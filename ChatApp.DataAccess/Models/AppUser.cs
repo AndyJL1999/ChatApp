6a9e8e08f830081d3a79808d9c7058707188e100
AndyJL1999 <andylainez115@hotmail.com>
2024-03-12 22:31:32 -0400

    --Added Email property to AppUser model --Added data annotation to Email property

--- ChatApp.DataAccess/Models/AppUser.cs
+++ ChatApp.DataAccess/Models/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,8 @@ namespace ChatApp.DataAccess.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
     }
 }
