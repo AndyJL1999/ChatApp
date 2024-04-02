c1e29887e7cd6146c647a13cd0a3bea21ab3d4fe
AndyJL1999 <andylainez115@hotmail.com>
2024-04-01 21:01:16 -0400

    --Added dropdown menu to sidebar --Created ChatController --Created GroupController --Created Create Views for Chat and Group folders --Removed Privacy View from Home folder

---
+++ ChatAppWeb/Areas/UserHome/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppWeb.Areas.UserHome.Controllers
+{
+    [Area("UserHome")]
+    [Authorize]
+    public class ChatController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
