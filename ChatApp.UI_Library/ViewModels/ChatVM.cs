a45a38546cb6a0d6e3dee5393949c68049354e4b
AndyJL1999 <andylainez115@hotmail.com>
2024-04-01 20:59:36 -0400

    --Created ChatVM --Created GroupVM

---
+++ ChatApp.UI_Library/ViewModels/ChatVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.ViewModels
+{
+    public class ChatVM
+    {
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ChatApp.UI_Library/ViewModels/GroupVM.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.ViewModels
+{
+    public class GroupVM
+    {
+        public string PhoneNumber { get; set; }
+        public IEnumerable<string>? Users { get; set; }
+    }
+}
