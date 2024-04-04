c02b8ac74f6d1b9b4a6bd3bf8a8f1a0b1ae079dc
AndyJL1999 <andylainez115@hotmail.com>
2024-04-03 23:12:48 -0400

    --Created ChatHelper --Created IChatHelper

---
+++ ChatApp.UI_Library/API/ChatHelper.cs
@@ -0,0 +1,36 @@
+﻿using ChatApp.UI_Library.API.Interfaces;
+using ChatApp.UI_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.API
+{
+    public class ChatHelper : IChatHelper
+    {
+        private readonly IApiHelper _apiHelper;
+
+        public ChatHelper(IApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<string> UpsertChat(string number)
+        {
+            var content = JsonContent.Create(number);
+
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsync(_apiHelper.ApiClient.BaseAddress + "Chat/CreateChat", content))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Chat created successfully!";
+                }
+
+                return "Something went wrong.";
+            }
+        }
+    }
+}
