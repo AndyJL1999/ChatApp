db877bbefd34b16fe961fe4e91a373c7a1841a54
AndyJL1999 <andylainez115@hotmail.com>
2024-04-03 23:09:29 -0400

    --Created static extension class Setup.cs --This class is for registering interfaces for dependency injection

---
+++ ChatApp.UI_Library/SetUp.cs
@@ -0,0 +1,22 @@
+﻿using ChatApp.UI_Library.API;
+using ChatApp.UI_Library.API.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library
+{
+    public static class SetUp
+    {
+        public static IServiceCollection AddLibraryServices(this IServiceCollection services)
+        {
+            return services
+                .AddSingleton<IApiHelper, ApiHelper>()
+                .AddScoped<IAuthHelper, AuthHelper>()
+                .AddScoped<IChatHelper, ChatHelper>();
+        }
+    }
+}
