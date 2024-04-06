9c79b75aa7914533f8146b38ec714f061a5fcdf7
AndyJL1999 <andylainez115@hotmail.com>
2024-04-05 20:14:43 -0400

    --Added Create POST method --Added constructor

--- ChatAppWeb/Areas/UserHome/Controllers/ChatController.cs
+++ ChatAppWeb/Areas/UserHome/Controllers/ChatController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ChatApp.UI_Library.API.Interfaces;
+using ChatApp.UI_Library.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatAppWeb.Areas.UserHome.Controllers
@@ -7,9 +9,29 @@ namespace ChatAppWeb.Areas.UserHome.Controllers
     [Authorize]
     public class ChatController : Controller
     {
+        private readonly IChatHelper _chatHelper;
+
+        public ChatController(IChatHelper chatHelper)
+        {
+            _chatHelper = chatHelper;
+        }
+
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ChatVM chatVM)
+        {
+            string result = await _chatHelper.UpsertChat(chatVM.PhoneNumber);
+
+            if (string.IsNullOrEmpty(result))
+            {
+                return View(chatVM);
+            }
+
+            return RedirectToAction("Index", "Home", new { area = "UserHome" });
         }
     }
 }
