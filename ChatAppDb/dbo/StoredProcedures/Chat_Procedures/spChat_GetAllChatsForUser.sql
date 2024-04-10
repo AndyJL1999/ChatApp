126ce8f49f6739ee6b562668ecf4cd22abc0767b
AndyJL1999 <andylainez115@hotmail.com>
2024-04-09 20:52:53 -0400

    --Created Chat_Procedures folder --Created spChat_GetAllChatsForUser.sql

---
+++ ChatAppDb/dbo/StoredProcedures/Chat_Procedures/spChat_GetAllChatsForUser.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spChat_GetAllChatsForUser]
+	@UserId nvarchar(450)
+AS
+begin
+	select [Chat].Id, [Chat].Name
+	from dbo.[Chat]
+	inner join dbo.[UserChat] on [Chat].Id = [UserChat].ChatId
+	where [UserChat].UserId = @UserId
+end
+
