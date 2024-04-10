bad4faf24cb756816f8ba81d3892dcdf4fe2c7a7
AndyJL1999 <andylainez115@hotmail.com>
2024-04-09 20:53:31 -0400

    --Created UserGroup_Procedures folder --Created spUserGroup_Insert.sql

---
+++ ChatAppDb/dbo/StoredProcedures/UserGroup_Procedures/spUserGroup_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUserGroup_Insert]
+	@Id nvarchar(450),
+	@UserId nvarchar(450),
+	@GroupId nvarchar(450)
+AS
+begin
+	insert into dbo.[UserGroup](Id, UserId, GroupId)
+	values(@Id, @UserId, @GroupId)
+end
