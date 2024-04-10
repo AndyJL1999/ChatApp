687481f1e1101b92991409212b80f8ff8ea0cd6d
AndyJL1999 <andylainez115@hotmail.com>
2024-04-09 20:52:06 -0400

    --Created Group_Procedures folder --Created spGroup_GetAllGroupsForUser.sql --Created spGroup_Upsert.sql

---
+++ ChatAppDb/dbo/StoredProcedures/Group_Procedures/spGroup_GetAllGroupsForUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spGroup_GetAllGroupsForUser]
+	@UserId nvarchar(450)
+AS
+begin 
+	select [Group].Id, [Group].Name
+	from dbo.[Group]
+	inner join dbo.[UserGroup] on [Group].Id = [UserGroup].GroupId
+	where [UserGroup].UserId = @UserId
+end
---
+++ ChatAppDb/dbo/StoredProcedures/Group_Procedures/spGroup_Upsert.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spGroup_Upsert]
+	@Id nvarchar(450),
+	@Name nvarchar(256)
+AS
+
+if((select 1 from dbo.[Group] where Id = @Id) = 1)
+begin
+	update dbo.[Group]
+	set Name = @Name
+	where Id = @Id;
+end
+else
+begin
+	insert into dbo.[Group](Id, Name)
+	values(@Id, @Name)
+end
