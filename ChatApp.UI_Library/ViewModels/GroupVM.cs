6ad7dd654643ad43e3adee5cc8d814e416a9ab08
AndyJL1999 <andylainez115@hotmail.com>
2024-04-10 22:42:27 -0400

    --Added GroupName property --Added Data annotations

--- ChatApp.UI_Library/ViewModels/GroupVM.cs
+++ ChatApp.UI_Library/ViewModels/GroupVM.cs
@@ -1,6 +1,8 @@
 ﻿using ChatApp.DataAccess.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,14 @@ namespace ChatApp.UI_Library.ViewModels
 {
     public class GroupVM
     {
+        [Required]
+        [DisplayName("Group Name")]
+        public string GroupName { get; set; }
+
+        [Phone(ErrorMessage = "Enter valid phone number")]
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
-        public IEnumerable<string>? Users { get; set; }
+
+        public List<string> PhoneNumbers { get; set; } 
     }
 }
