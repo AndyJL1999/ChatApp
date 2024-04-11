9b2c838236eab5a99100d4563e818f43f86d1279
AndyJL1999 <andylainez115@hotmail.com>
2024-04-10 22:42:41 -0400

    --Added data annotations

--- ChatApp.UI_Library/ViewModels/ChatVM.cs
+++ ChatApp.UI_Library/ViewModels/ChatVM.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace ChatApp.UI_Library.ViewModels
 {
     public class ChatVM
     {
+        [Phone(ErrorMessage = "Enter valid phone number")]
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
     }
 }
