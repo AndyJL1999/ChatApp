efeaf7b0a086a4c346a8e9ecf8b441eaa0c9bc88
AndyJL1999 <andylainez115@hotmail.com>
2024-04-10 22:40:33 -0400

    --Created UserHelper --Created GetAllUserChannels method

---
+++ ChatApp.UI_Library/API/Interfaces/IUserHelper.cs
@@ -0,0 +1,14 @@
+﻿using ChatApp.UI_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.API.Interfaces
+{
+    public interface IUserHelper
+    {
+        Task<IEnumerable<ChannelModel>> GetAllUserChannels(); 
+    }
+}
---
+++ ChatApp.UI_Library/API/UserHelper.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.UI_Library.API.Interfaces;
+using ChatApp.UI_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.API
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly IApiHelper _apiHelper;
+
+        public UserHelper(IApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<IEnumerable<ChannelModel>> GetAllUserChannels()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync(_apiHelper.ApiClient.BaseAddress + "User/GetAllChannels"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadFromJsonAsync<IEnumerable<ChannelModel>>();
+
+                    return result;
+                }
+
+                return null;
+            }
+        }
+    }
+}
