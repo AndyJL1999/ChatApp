0c45ce0ac168ba95c1f0d6befc5d76132cc61af5
AndyJL1999 <andylainez115@hotmail.com>
2024-04-11 21:14:00 -0400

    --Added IChannel to Chat and Group class

--- ChatApp.DataAccess/Models/Chat.cs
+++ ChatApp.DataAccess/Models/Chat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatApp.DataAccess.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ChatApp.DataAccess.Models
 {
-    public class Chat
+    public class Chat : IChannel
     {
         public string Id { get; set; }
         public string Name { get; set; }
