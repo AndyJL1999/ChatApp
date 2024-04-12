9818214aeb484adfb6e6b53e92b429d08417de43
AndyJL1999 <andylainez115@hotmail.com>
2024-04-12 17:41:16 -0400

    --Created spMessage_GetAllFromChannel.sql

---
+++ ChatAppDb/dbo/StoredProcedures/Message_Procedures/spMessage_GetAllFromChannel.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_GetAllFromChannel]
+	@ChannelId nvarchar(450)
+AS
+begin
+	select [Message].Id, [Message].UserId, [Message].Content, [Message].SentAt 
+	from dbo.[Message]
+	where [Message].ChatId = @ChannelId or [Message].GroupId = @ChannelId
+	order by [Message].SentAt
+end
