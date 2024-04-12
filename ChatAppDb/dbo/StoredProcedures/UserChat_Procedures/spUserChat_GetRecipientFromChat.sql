5ce7c1fdca32d76cbc34884a6d2bf3d7d795cd6f
AndyJL1999 <andylainez115@hotmail.com>
2024-04-11 21:09:49 -0400

    --Created spUserChat_GetRecipientFromChat.sql

---
+++ ChatAppDb/dbo/StoredProcedures/UserChat_Procedures/spUserChat_GetRecipientFromChat.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUserChat_GetRecipientFromChat]
+	@CurrentUserId nvarchar(450),
+	@ChatId nvarchar(450)
+AS
+begin
+	select [UserChat].UserId
+	from dbo.[UserChat]
+	where ChatId = @ChatId and UserId != @CurrentUserId
+end
