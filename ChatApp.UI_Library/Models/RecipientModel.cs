1ea4086c7e064a04bbba9af39e8ef989ba4b2eb6
AndyJL1999 <andylainez115@hotmail.com>
2024-04-12 17:45:53 -0400

    --Created RecipientModel.cs

---
+++ ChatApp.UI_Library/Models/RecipientModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.Models
+{
+    public class RecipientModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
