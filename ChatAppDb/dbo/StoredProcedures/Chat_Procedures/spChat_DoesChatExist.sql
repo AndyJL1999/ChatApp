a4b12a7560ce8950a27fabf6ac9b222ab87147fe
AndyJL1999 <andylainez115@hotmail.com>
2024-04-17 22:17:11 -0400

    --Created spChat_DoesChatExist

---
+++ ChatAppDb/dbo/StoredProcedures/Chat_Procedures/spChat_DoesChatExist.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spChat_DoesChatExist]
+	@UserId nvarchar(450),
+	@RecipientId nvarchar(450)
+AS
+begin 
+	select case when exists (select [UserChat].ChatId 
+	from dbo.[UserChat] 
+	where [UserChat].UserId = @UserId or [UserChat].UserId = @RecipientId
+	group by [UserChat].ChatId 
+	having count(*) > 1)
+	then cast(1 as bit) else cast(0 as bit) end as 'DoesExist'
+end
