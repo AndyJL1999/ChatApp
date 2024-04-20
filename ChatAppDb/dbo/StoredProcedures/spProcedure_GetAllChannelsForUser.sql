0d21ec5e696e39a78cb0967fe8d5a935a99ce275
AndyJL1999 <andylainez115@hotmail.com>
2024-04-19 23:23:51 -0400

    --Created spProcedure_GetAllChannelsForUser

---
+++ ChatAppDb/dbo/StoredProcedures/spProcedure_GetAllChannelsForUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spProcedure_GetAllChannelsForUser]
+	@UserId nvarchar(450)
+
+AS
+begin
+	select [Chat].Id, [Chat].Name, 'Chat' as 'Type'
+	from dbo.[Chat]
+	inner join dbo.[UserChat] on [Chat].Id = [UserChat].ChatId
+	where [UserChat].UserId = @UserId
+	union
+	select [Group].Id, [Group].Name, 'Group' as 'Type'
+	from dbo.[Group]
+	inner join dbo.[UserGroup] on [Group].Id = [UserGroup].GroupId
+	where [UserGroup].UserId = @UserId
+	order by 'Type' desc
+end
