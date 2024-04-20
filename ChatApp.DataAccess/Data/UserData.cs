4ca9d36359d5bc8632809298fa19a462e0257bc3
AndyJL1999 <andylainez115@hotmail.com>
2024-04-19 23:24:36 -0400

    --Created UserData

---
+++ ChatApp.DataAccess/Data/UserData.cs
@@ -0,0 +1,23 @@
+﻿using ChatApp.DataAccess.DataAccess;
+using ChatApp.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Data
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<(string Id, string Name, string Type)>> GetAllChannels(string userId) =>
+            _db.LoadData<(string Id, string Name, string Type), dynamic>("spProcedure_GetAllChannelsForUser", new { UserId = userId });
+    }
+}
---
+++ ChatApp.DataAccess/Interfaces/IUserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Interfaces
+{
+    public interface IUserData
+    {
+        Task<IEnumerable<(string Id, string Name, string Type)>> GetAllChannels(string userId);
+    }
+}
