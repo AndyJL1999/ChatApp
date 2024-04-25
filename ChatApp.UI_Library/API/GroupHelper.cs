65f0538594db32e53a7fad67d7637cccfd46d790
AndyJL1999 <andylainez115@hotmail.com>
2024-04-24 21:18:19 -0400

    --Created GroupHelper

---
+++ ChatApp.UI_Library/API/GroupHelper.cs
@@ -0,0 +1,40 @@
+﻿using ChatApp.UI_Library.API.Interfaces;
+using ChatApp.UI_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.API
+{
+    public class GroupHelper : IGroupHelper
+    {
+        private readonly IApiHelper _apiHelper;
+
+        public GroupHelper(IApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<NewGroupModel> UpsertGroup(string groupName, List<string> phoneNumbers)
+        {
+            var content = JsonContent.Create(new
+            {
+                GroupName = groupName,
+                PhoneNumbers = phoneNumbers
+            });
+
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsync(_apiHelper.ApiClient.BaseAddress + "Group/CreateGroup", content))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<NewGroupModel>();
+                }
+
+                return null;
+            }
+        }
+    }
+}
