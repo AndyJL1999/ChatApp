da29c24e98b222cbe29bfb3abff47811e72bda89
AndyJL1999 <andylainez115@hotmail.com>
2024-04-24 21:18:58 -0400

    ----New models --Created NewChatModel --Created NewGroupModel

---
+++ ChatApp.UI_Library/Models/NewChatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.Models
+{
+    public class NewChatModel
+    {
+        public string ChatId { get; set; }
+        public string RecipientId { get; set; }
+        public string RecipientName { get; set; }
+    }
+}
---
+++ ChatApp.UI_Library/Models/NewGroupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.UI_Library.Models
+{
+    public class NewGroupModel
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+
+    }
+}
