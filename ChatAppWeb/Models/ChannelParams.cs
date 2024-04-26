68d4529c428f48b09a992a120b0374f597ec7630
AndyJL1999 <andylainez115@hotmail.com>
2024-04-26 19:53:20 -0400

    --Created ChannelParams class --Used for receiving data from client to server via AJAX

---
+++ ChatAppWeb/Models/ChannelParams.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.UI_Library.Models;
+
+namespace ChatAppWeb.Models
+{
+    public class ChannelParams
+    {
+        public string ChannelId { get; set; }
+        public List<MessageModel> Messages { get; set; }
+    }
+}
