d789913103355542155d44e5f3162f4df0c08e44
AndyJL1999 <andylainez115@hotmail.com>
2024-04-29 20:30:46 -0400

    -- Created GetMessagesDTO

---
+++ ChatAppAPI/DTOs/GetMessagesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.API.DTOs
+{
+    public class GetMessagesDTO
+    {
+        public string ChannelId { get; set; }
+        public int Limit { get; set; } = 20;
+        public int Offset { get; set; } = 0;
+    }
+}
