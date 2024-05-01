e2cb68c9870f0fc9e5fa142b3374870155e1761f
AndyJL1999 <andylainez115@hotmail.com>
2024-04-30 20:03:24 -0400

    --Created connectSignalR.js for chat.js and group.js

---
+++ ChatAppWeb/wwwroot/js/connectSignalR.js
@@ -0,0 +1,9 @@
+﻿var connection = new signalR.HubConnectionBuilder()
+    .withUrl(hubUrl).build();
+
+connection.start().catch(error => console.log(error));
+
+function sendMessageToHub(message) {
+    connection.invoke('SendMessage', message)
+        .catch(error => console.log(error));
+}
