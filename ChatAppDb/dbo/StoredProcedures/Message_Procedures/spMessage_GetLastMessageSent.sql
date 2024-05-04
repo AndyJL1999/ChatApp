661a2261d40bd231599e482fe50d6d5ed471f8a0
AndyJL1999 <andylainez115@hotmail.com>
2024-05-03 22:05:09 -0400

    --Created spMessage_GetLastMessageSent.sql

---
+++ ChatAppDb/dbo/StoredProcedures/Message_Procedures/spMessage_GetLastMessageSent.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_GetLastMessageSent]
+	@ChannelId nvarchar(450)
+AS
+
+begin
+	select top 1 [Message].Content
+	from dbo.[Message]
+	where [Message].ChatId = @ChannelId or [Message].GroupId = @ChannelId
+	order by [Message].SentAt desc
+end
