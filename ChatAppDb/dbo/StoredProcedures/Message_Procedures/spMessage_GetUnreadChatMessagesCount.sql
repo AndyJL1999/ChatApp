56e1413cdfc692fd2f008a5127f5cc81b1a654ac
AndyJL1999 <andylainez115@hotmail.com>
2024-05-17 21:04:40 -0400

    --Created spMessage_GetUnreadChatMessagesCount

---
+++ ChatAppDb/dbo/StoredProcedures/Message_Procedures/spMessage_GetUnreadChatMessagesCount.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_GetUnreadChatMessagesCount]
+	@ChatId nvarchar(450),
+	@UserId nvarchar(450)
+AS
+begin
+	select COUNT([Message].Id)
+	from dbo.[Message]
+	where [Message].ChatId = @ChatId and [Message].UserId != @UserId and [Message].SeenAt is null
+end
