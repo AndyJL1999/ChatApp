22adb27c89d8485c6a220b9cddaca02f45adc848
AndyJL1999 <andylainez115@hotmail.com>
2024-05-28 19:29:34 -0400

    --Modified Channels initialization --Modified scripts structure --Added presence.js

---
+++ ChatAppWeb/wwwroot/js/presence.js
@@ -0,0 +1,29 @@
+﻿
+function getToken() {
+
+    return new Promise(function (resolve) {
+        $.ajax({
+            type: "GET",
+            url: "/UserAccount/Auth/GetTokenForPresenceHub",
+            data: {},
+            contentType: "application/json",
+            success: function (data) {
+                if (data.success) {
+                    resolve(data.token);
+                }
+            }
+        });
+    });
+}
+
+getToken().then(function (token) {
+    var connection = new signalR.HubConnectionBuilder()
+        .withUrl(hubUrl + 'presence', {
+            accessTokenFactory: () => token
+        })
+        .withAutomaticReconnect()
+        .build();
+
+    connection.start().catch(error => console.log(error));
+})
+
