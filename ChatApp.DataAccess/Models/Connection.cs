96e8c771c265844abc7c5a0a24b2ea977e7f7a6b
AndyJL1999 <andylainez115@hotmail.com>
2024-06-04 18:57:29 -0400

    --Created Connection model

---
+++ ChatApp.DataAccess/Models/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Models
+{
+    public class Connection
+    {
+        public string ConnectionId { get; set; }
+        public string ChannelId { get; set; }
+        public string Username { get; set; }
+    }
+}
